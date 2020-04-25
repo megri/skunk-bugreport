8eb6b86fa16616e4e30454530e7f10ace66ce2c7
Martin <teknocide@gmail.com>
2020-04-25 14:29:45 +0200

    try natchez-log

--- build.sbt
+++ build.sbt
@@ -2,11 +2,14 @@ ThisBuild / scalaVersion := "2.13.1"
 
 lazy val core = project.in(file("."))
   .settings(
+    addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full),
     libraryDependencies ++= Seq(
       "org.flywaydb" % "flyway-core" % "6.3.2",
       "org.postgresql" % "postgresql" % "42.2.12",
       "ch.qos.logback" % "logback-classic" % "1.2.3",
       "org.tpolecat" %% "skunk-core" % "0.0.8",
+      "org.tpolecat" %% "natchez-log" % "0.0.11",
+       "io.chrisdavenport" %% "log4cats-slf4j" % "1.0.1",
     ),
     run / fork := true,
   )
--- src/main/scala/com/megri/skunktest/BugReport.scala
+++ src/main/scala/com/megri/skunktest/BugReport.scala
@@ -1,51 +1,74 @@
 package com.megri.skunktest
 
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration._
+
+import cats._
+import cats.data.Kleisli
 import cats.effect._
-import cats.syntax.all._
+import cats.implicits._
+
+import natchez._
 
 import org.flywaydb.core.Flyway
-
-import natchez.Trace.Implicits.noop
 
 import skunk._
 import skunk.codec.all._
 import skunk.implicits._
 
 object BugReport extends IOApp {
-  val session: Resource[IO, Session[IO]] =
+
+  def entryPoint[F[_]: Sync]: Resource[F, EntryPoint[F]] = {
+    import natchez.log.Log
+    import io.chrisdavenport.log4cats.Logger
+    import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+
+    implicit val log: Logger[F] = Slf4jLogger.getLogger[F]
+    Log.entryPoint[F]("foo").pure[Resource[F, *]]
+  }
+
+  def migrate[F[_]](implicit F: Sync[F]): F[Unit] = {
+    val url = s"jdbc:postgresql://localhost:5432/postgres"
+    for {
+      flyway <- F.delay(Flyway.configure().dataSource(url, "postgres", null).load())
+      _      <- F.delay(flyway.clean())
+      _      <- F.delay(flyway.migrate())
+    } yield ()
+  }
+
+  def session[F[_]: Concurrent: ContextShift: Trace]: Resource[F, Session[F]] =
     Session.single(
       host = "localhost",
       port = 5432,
       user = "postgres",
-      database = "postgres"
+      database = "postgres",
+      debug = true,
+      readTimeout = 3.seconds,
+      writeTimeout = 3.seconds
     )
 
-  val migrate = {
-    val url = s"jdbc:postgresql://localhost:5432/postgres"
-    for {
-      flyway <- IO(Flyway.configure().dataSource(url, "postgres", null).load())
-      _      <- IO(flyway.clean())
-      _      <- IO(flyway.migrate())
-    } yield ()
-  }
+  val badQuery: Query[Long, Long ~ Long] =
+    sql"""INSERT INTO "foo" ("bar_id") VALUES ($int8) RETURNING *""".query(int8 ~ int8)
 
-  val badQuery =
-    sql"""INSERT INTO foo ("bar_id") VALUES ($int8) RETURNING *""".query(int8 ~ int8)
+  def runQuery[F[_]: Concurrent: ContextShift: Trace] =
+    session.use(_.prepare(badQuery).use(_.unique(42)))
 
-  val runQuery = session.use(_.prepare(badQuery).use(_.unique(42)))
+  def runF[F[_]: Concurrent: ContextShift: Trace]: F[Either[Throwable, Long ~ Long]] =
+    Trace[F].span("running query") {
+      for {
+        _ <- migrate
+        r <- runQuery.attempt
+      } yield r
+    }
 
   def run(args: List[String]): IO[ExitCode] = {
-    val result = for {
-      _ <- migrate
-      _ <- IO(println("Starting..."))
-      r <- runQuery.attempt // hangs here
-      _ <- IO(println("Done!"))
-    } yield r
-
-    // never happens
-    result.flatMap {
-      case Left(error) => IO(println(s"error: ${error.getMessage}")).as(ExitCode.Error)
-      case Right(fooId ~ barId) => IO(println(s"success: data ($fooId, $barId)")).as(ExitCode.Success)
+    entryPoint[IO].use { ep =>
+      ep.root("entrypoint").use { span =>
+        runF[Kleisli[IO, Span[IO], *]].run(span).flatMap {
+          case Left(err)   => IO(println(s"Failure: $err")).as(ExitCode.Error)
+          case Right(data) => IO(println(s"Success: $data")).as(ExitCode.Success)
+        }
+      }
     }
   }
 }
